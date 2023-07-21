f1bf1d112943f00c4862e804a614c3ed3e844776
Helen Ivanova <helen.zakhmatova@mail.ru>
2023-07-21 13:20:45 +0400

    hometask for seminar 7

---
+++ task1/Program.cs
@@ -0,0 +1,59 @@
+﻿/* Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+ */
+//Random rnd = new Random();
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double ReadDouble(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double[,] GenerateArray2D(int rows, int cols, double maxValue, double minValue)
+{
+    double[,] array = new double[rows, cols];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(new Random().NextDouble() * (maxValue - minValue) + minValue, 2);
+        }
+    }
+    return array;
+}
+
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+
+
+
+
+int rows = ReadInt("Введите количество строк в массиве => ");
+int cols = ReadInt("Введите количество столбцов в массиве => ");
+
+double minValue = ReadDouble("Введите минимальную границу диапазона -> ");
+double maxValue = ReadDouble("Введите максимальную границу диапазона -> ");
+double[,] currentarray2D = GenerateArray2D(rows, cols, minValue, maxValue);
+PrintArray2D(currentarray2D);
---
+++ task2/Program.cs
@@ -0,0 +1,81 @@
+﻿/* Задача 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+1,7 -> такого числа в массиве нет
+1,2 -> 4 */
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateArray2D(int rows, int cols, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, cols];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(minValue , maxValue);
+        }
+    }
+    return array;
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+string ReturnItem(int[,] array, int row, int col)
+{
+    string s = "";
+    if (row > array.GetLength(0) || col > array.GetLength(1))
+    {
+        s = "Такого элемента нет";
+    }
+    else
+    {
+         for (int i = 0; i < array.GetLength(0) + 1; i++)
+        {
+            for (int j = 0; j < array.GetLength(1) + 1; j++)
+            {
+                if (i + 1 == row && j + 1 == col)
+                {
+                    System.Console.WriteLine($"Элемент, расположенный на позициях ({row}, {col}) = {array[i, j]}");
+                }
+            }   
+        }
+    }   
+    return s;
+}
+
+
+int rows = ReadInt("Введите количество строк в массиве => ");
+int cols = ReadInt("Введите количество столбцов в массиве => ");
+int minValue = ReadInt("Введите минимальную границу диапазона -> ");
+int maxValue = ReadInt("Введите максимальную границу диапазона -> ");
+int[,] currentarray2D = GenerateArray2D(rows, cols, minValue, maxValue);
+PrintArray2D(currentarray2D);
+int numberRow = ReadInt("Введите номер строки -> ");
+int numberCol = ReadInt("Введите номер столбца -> ");
+System.Console.WriteLine(ReturnItem(currentarray2D, numberRow, numberCol));
+
+
+    
+
+
